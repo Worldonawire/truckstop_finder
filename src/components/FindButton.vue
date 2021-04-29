95e599c2902547faea106f1b5b83f4ef01d14fe4
Russell Pacheco <russ1487@gmail.com>
2021-04-29 12:00:55 +0900

    Working on adding the Find button component

---
+++ src/components/FindButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button className="Trucker">Find Truck Stops</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FindButton",
+};
+</script>
+
+<style scoped>
+button {
+  height: 100px;
+  width: 100px;
+  background-color: #bbb;
+  border-radius: 50%;
+}
+</style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,33 +1,44 @@
 <template>
-  <GmapMap
-    :zoom="4"
-    :center="{ lat: 25.7392, lng: -104.9903 }"
-    map-type-id="terrain"
-    style="width: 100%; height: 300px"
-  >
-    <GmapMarker
-      v-for="location in locations"
-      :key="location.key"
-      :position="location.position"
-      :animation="location.defaultAnimation"
-      @rightclick="markerRightClicked"
-    />
-  </GmapMap>
+  <div>
+    <GmapMap
+      :zoom="5"
+      :center="{ lat: 39.5, lng: -98.35 }"
+      map-type-id="terrain"
+      style="width: 100%; height: 300px"
+    >
+      <GmapMarker
+        v-for="location in locations"
+        :key="location.key"
+        :position="location.position"
+        :animation="location.defaultAnimation"
+        @rightclick="markerRightClicked"
+      />
+      <FindButton />
+    </GmapMap>
+  </div>
 </template>
 
 <script>
 import { gmapApi } from "vue2-google-maps";
+import FindButton from "./FindButton.vue";
 
 export default {
+  name: "Map",
+  components: {
+    FindButton,
+  },
+
   mounted() {
     this.getLocations();
   },
+
   computed: {
     locations() {
       return this.$store.state.locations;
     },
     google: gmapApi,
   },
+
   methods: {
     getLocations() {
       this.$store.dispatch("loadMarkers");
@@ -36,3 +47,4 @@ export default {
   },
 };
 </script>
+<style scoped></style>
