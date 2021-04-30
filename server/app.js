056e1ed3af26c4196d22a0927ad191c411003553
GaetanKarst <gaetan.karst@gmail.com>
2021-04-30 14:22:13 +0900

    fix server root + some endpoints completed

--- server/app.js
+++ server/app.js
@@ -1,31 +1,127 @@
 const express = require("express");
-const morgan = require("morgan");
 const path = require("path");
-// const db = require("knex");
-const api = require("./routes/api");
+const db = require("./knex");
 
 const app = express();
 
-// Setup Logger
 app.use(express.json());
-app.use("/api", api);
-app.use(
-  morgan(
-    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
-  )
-);
-
-// app.get("/", ((req, res) => {
-// }));
 
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, "..", "dist")));
 
-// Always return the main index.html, since we are developing a single page application
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
-// });
+//Always return the main index.html, since we are developing a single page application
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
+});
+
+app.get("/api", (req, res) => {
+  const queryKey = Object.keys(req.query);
+  const queryValue = req.query[queryKey];
+
+  // allows to use a single endpoint for queries
+  function redirect(queryKey, queryValue) {
+    console.log(queryKey);
+    switch (queryKey) {
+      case "type":
+        console.log("Routing to 'Type'...");
+        type(queryValue);
+        break;
+      case "city":
+        console.log("Routing to 'City'...");
+        city(queryValue);
+        break;
+      case "state":
+        console.log("Routing to 'State'...");
+        state(queryValue);
+        break;
+      case "amenities":
+        console.log("Routing to 'Amenities'...");
+        amenities(queryValue);
+        break;
+      case "payments":
+        console.log("Routing to 'Payments'...");
+        payments(queryValue);
+        break;
+      case "restaurants":
+        console.log("Routing to 'Restaurants'...");
+        restaurants(queryValue);
+        break;
+      default:
+        console.log("Switch failed ");
+        res.status(400).end();
+        break;
+    }
+  }
+
+  redirect(queryKey.toString(), queryValue);
+
+  async function type(queryValue) {
+    try {
+      const allMatches = await db("locations").where("type", queryValue);
+      res.status(200);
+      res.send(allMatches);
+    } catch (e) {
+      console.log(e);
+      res.status(400).end();
+    }
+  }
+
+  async function state(queryValue) {
+    try {
+      const allMatches = await db("locations").where("state", queryValue);
+      res.status(200);
+      res.send(allMatches);
+    } catch (e) {
+      console.log(e);
+      res.status(400).end();
+    }
+  }
+
+  async function city(queryValue) {
+    try {
+      const allMatches = await db("locations").where("city", queryValue);
+      res.status(200);
+      res.send(allMatches);
+    } catch (e) {
+      console.log(e);
+      res.status(400).end();
+    }
+  }
+
+  async function amenities(queryValue) {
+    try {
+      const allMatches = await db("locations").where("amenities", queryValue);
+      res.status(200);
+      res.send(allMatches);
+    } catch (e) {
+      console.log(e);
+      res.status(400).end();
+    }
+  }
+
+  async function payments(queryValue) {
+    try {
+      const allMatches = await db("locations").where("payments", queryValue);
+      res.status(200);
+      res.send(allMatches);
+    } catch (e) {
+      console.log(e);
+      res.status(400).end();
+    }
+  }
+
+  async function restaurants(queryValue) {
+    try {
+      const allMatches = await db("locations").where("restaurants", queryValue);
+      res.status(200);
+      res.send(allMatches);
+    } catch (e) {
+      console.log(e);
+      res.status(400).end();
+    }
+  }
+});
 
 module.exports = app;
 
-// /api/type/?type="Country Store"
+// we need to unbreak the root at some point
