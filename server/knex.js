718933df98834093678c698b83265e74ebc1d129
RussellPacheco <65903196+RussellPacheco@users.noreply.github.com>
2021-05-01 19:39:59 +0900

    Update knex.js

--- server/knex.js
+++ server/knex.js
@@ -6,6 +6,7 @@ const db = knex({
     process.env.DATABASE_URL ||
     `postgres://${process.env.USER}@127.0.0.1:5432/truckstop`,
   searchPath: "public",
+  ssl: { rejectUnauthorized: false },  
 });
 
 module.exports = db;
